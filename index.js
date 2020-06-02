7d4236f975822dc2e727284fcb6da806d4ac0e6d
Cattleya Pia <cjpia612@gmail.com>
2020-06-02 00:42:08 -0700

    created questions array

--- index.js
+++ index.js
@@ -5,9 +5,45 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 const util = ("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
-
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "Username",
+        message: "Enter your GitHub username"
+    },
+    {
+        type: "input",
+        name: "Title",
+        message: "Enter desired title for project"
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Enter a short description of project"
+    },
+    {
+        type: "input",
+        name: "Installation ",
+        message: "Enter desired title for project"
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "Provide user instructions"
+    },
+    {
+        type: "input",
+        name: "Contributing",
+        message: "Enter names of project collaborators"
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "Include any requirements to run tests"
+    },
+
+
 
 ];
 
@@ -17,7 +53,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions);
 }
 
 // function call to initialize program
