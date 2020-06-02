6bca2e4cd34c8a0536aeb640f01eb3adfc0d616d
Cattleya Pia <cjpia612@gmail.com>
2020-06-01 20:56:19 -0700

    added consts

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const util = ("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
 // array of questions for user
 const questions = [
 
