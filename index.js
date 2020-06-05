cd17ec5ca977970683be4cf3c3bc924f821e021f
Cattleya Pia <cjpia612@gmail.com>
2020-06-04 22:37:24 -0700

    fixed defined variables

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown.js");
-const util = ("util");
+const generateMarkdown = require("./utils/generateMarkdown");
+const util = require("util");
+
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
